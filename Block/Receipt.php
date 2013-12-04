19f378b4432c3117b688487ff4c28654d6c0657a
Lucas Guarnieri <guarnieri@cammino.com.br>
2013-12-04 09:30:32 -0200

    Update Order status, if is processing or canceled

--- Block/Receipt.php
+++ Block/Receipt.php
@@ -30,9 +30,30 @@ class Cammino_Cielo_Block_Receipt extends Mage_Payment_Block_Form {
 	public function getStatusPedido()
 	{
 		if (!$this->_orderId) { return false; }
-
+		
+		$order = Mage::getModel('sales/order')->loadByIncrementId($this->_orderId);
 		$cielo = Mage::getModel('cielo/default');
-		return $cielo->sendXml($this->_orderId, 'receipt');
+		$xml = $cielo->sendXml($this->_orderId, 'receipt');
+		
+
+		if ($xml->status) {
+			if ( $xml->status == 1 || $xml->status == 2 || $xml->status == 4 || $xml->status == 6 || $xml->status == 10 ){
+				$state   = 'processing';
+				$status  = 'processing';
+				$comment = 'Processando pedido.'
+			} else{
+				$state = 'canceled';
+				$status = 'canceled'; //status criado por nós anteriormente.
+				if ( isset($xml->cancelamentos->cancelamento->mensagem) ) $comment = $xml->cancelamentos->cancelamento->mensagem; else $comment = 'Pedido cancelado pelo sistema Cielo.';
+			}
+		}
+
+			$order->setState($state, $status, $comment, false);
+			$order->save();
+
+
+
+		return $xml;
 	}
 
 }
