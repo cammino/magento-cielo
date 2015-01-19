99d4bca612f32ee2d4af8800ef9e74947107ec3d
Marcelo Ferracioli <marcelo@cammino.com.br>
2015-01-19 17:03:49 -0200

    trata transacoes com status 0

--- Block/Receipt.php
+++ Block/Receipt.php
@@ -42,6 +42,10 @@ class Cammino_Cielo_Block_Receipt extends Mage_Payment_Block_Form {
 				$state   = 'pending_payment';
 				$status  = 'pending_payment';
 				$comment = 'Cartão aprovado, aguardando captura.';
+			} else if (strval($xml->status) == "0") {
+				$state   = 'pending_payment';
+				$status  = 'pending_payment';
+				$comment = 'Transação criada na Cielo. Aguardando autorização.';
 			} else {
 				$state = 'canceled';
 				$status = 'canceled';
