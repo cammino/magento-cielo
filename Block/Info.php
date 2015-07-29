620ddf11607a85a4ee755eef9e7731f7352b0561
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2015-07-29 15:43:38 -0300

    Adiciona novos metodos para exibicao de valores nas tela de informacoes do pagamento

--- Block/Info.php
+++ Block/Info.php
@@ -1,9 +1,24 @@
 <?php
 class Cammino_Cielo_Block_Info extends Mage_Payment_Block_Info {
-	
+    
+    protected $_infoCielo;
+
     protected function _construct()
     {
         parent::_construct();
+
+        $this->setTemplate('cielo/info.phtml');
+    }
+
+    protected function _convertAdditionalData() {
+
+        $details = @unserialize( $this->getInfo()->getData("additional_data") );
+
+        if (is_object($details)) {
+             $this->_infoCielo = $details;
+        }
+        
+        return $this;
     }
 
     public function getOrder() {
@@ -14,8 +29,38 @@ class Cammino_Cielo_Block_Info extends Mage_Payment_Block_Info {
         return $this->getOrder()->getRealOrderId();
     }
 
-	public function getPayUrl() {
-		return Mage::getUrl('cielo/pay', array('id' => $this->getOrderId()));
-	}
-	
+    public function getPayUrl() {
+        return Mage::getUrl('cielo/pay', array('id' => $this->getOrderId()));
+    }
+
+    /**
+    * @return Object
+    **/
+    public function getInfoCielo() {
+        
+        if (empty($this->_infoCielo)) {
+            $this->_convertAdditionalData();
+        }
+
+        return $this->_infoCielo;
+    }
+
+    /**
+    * @return String
+    **/
+    public function getCieloPlotValue($plots) {
+        
+        $grand_total = 0;
+
+        if ($this->getOrder()) {
+            $grand_total = $this->getOrder()->getGrandTotal();
+        } else {
+            $totals = $this->getInfo()->getQuote()->getTotals();
+            $grand_total = $totals['grand_total']->_data['value'];
+        }
+
+        $total_plot  = ($plots) ? $grand_total / $plots : $grand_total;
+
+        return Mage::helper('core')->currency($total_plot, true, false);
+    }
 }
